b45d2cca8a3b548cff99d2cc8fc2de82ca66da49
Mangesh Ramesh Kadam <mangeshrameshkadam@Mangeshs-MacBook-Pro.local>
2024-08-22 11:30:16 +0530

    Mangesh Kadam: Updated code to complete assessment

--- limit/limit_order_agent.py
+++ limit/limit_order_agent.py
@@ -6,11 +6,33 @@ class LimitOrderAgent(PriceListener):
 
     def __init__(self, execution_client: ExecutionClient) -> None:
         """
-
         :param execution_client: can be used to buy or sell - see ExecutionClient protocol definition
         """
         super().__init__()
+        # set limit of share price to 100 and share will get buy price <= 100 and sell > 100
+        self.limit = None
+        self.stocks = []
+        self.amount = None
 
     def on_price_tick(self, product_id: str, price: float):
         # see PriceListener protocol and readme file
-        pass
+        # To buy 1000 shares of IBM when price <= 100
+        self.limit = 100
+        self.amount = 1000
+        self.add_order(True, product_id, self.amount, price)
+
+        # To sell 1000 shares of IBM when price > 100
+        # self.amount = 1000
+        # self.add_order(False, product_id, self.amount, price)
+
+    def add_order(self, buy_flag: bool, product_id:str, amount:float, price:float):
+        order = {'product_id': product_id, 'amount': amount, 'price': price, 'buy': buy_flag, 'sell': not buy_flag}
+        if buy_flag and price <= self.limit:
+            self.buy(product_id, amount)
+        elif not buy_flag and price > self.limit:
+            self.sell(product_id, amount)
+        # I have created self.stocks to maintain order data
+        self.stocks.append(order)
+
+
+
--- limit/tests/limit_order_agent_tests.py
+++ limit/tests/limit_order_agent_tests.py
@@ -1,7 +1,21 @@
 import unittest
+from ..limit_order_agent import LimitOrderAgent
 
 class LimitOrderAgentTest(unittest.TestCase):
 
+    def setUp(self):
+        self.order_agent = LimitOrderAgent()
+
     def test_something(self):
-        self.fail("not implemented")
+        # To test buy functionality
+        self.agent.add_order(True, 'IBM', 1000, 100)
+        self.assertEqual(len(self.agent.orders), 1)
+        self.assertEqual(self.agent.orders[0]['action'], True)
+        self.assertEqual(self.agent.orders[0]['product_id'], 'IBM')
+        self.assertEqual(self.agent.orders[0]['amount'], 1000)
+        self.assertEqual(self.agent.orders[0]['limit'], 100)
+
+
+
+
 
